f86a838f24f3b9e961e41a7576aa99c7e213d89c
JNDJ0 <joaomoreira@dcc.ufmg.br>
2026-06-21 06:34:32 -0300

    fix: LoginError retorna HTTP 409 em vez de 500

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -5,6 +5,7 @@ import { NotAuthorizedError } from '../../errors/NotAuthorizedError';
 import { InvalidParamError } from '../../errors/InvalidParamError';
 import { TokenError } from '../../errors/TokenError';
 import { QueryError } from '../../errors/QueryError';
+import { LoginError } from '../../errors/LoginError';
 
 export function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
     const message = error.message;
@@ -25,6 +26,10 @@ export function errorHandler(error: Error, req: Request, res: Response, next: Ne
 
     if (error instanceof QueryError) {
         status = statusCodes.BAD_REQUEST;
+    }
+
+    if (error instanceof LoginError) {
+        status = statusCodes.CONFLICT;
     }
 
     console.log(error);
